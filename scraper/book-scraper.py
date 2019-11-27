3a31095a0ce493a1331cf36a8b3fb74e7cd3163f
Daniel <KasumiL5x@gmail.com>
2019-11-27 19:33:18 +0000

    Updated the scraper to handle situations where rating and review count are singular.  Updated the CSV/TSV files too.

--- scraper/book-scraper.py
+++ scraper/book-scraper.py
@@ -103,13 +103,13 @@ class GoodReadsSpider(scrapy.Spider):
 		book_metas = selector.css('#bookMeta > a::text').getall()
 		#
 		# Ratings (contains `ratings`).
-		num_ratings_idx = [i for i,s in enumerate(book_metas) if 'ratings' in s]
+		num_ratings_idx = [i for i,s in enumerate(book_metas) if ('ratings' in s or 'rating' in s)]
 		if len(num_ratings_idx):
-			book_num_ratings = book_metas[num_ratings_idx[0]].replace('\n', '').replace(',', '').replace('ratings', '').strip()
+			book_num_ratings = book_metas[num_ratings_idx[0]].replace('\n', '').replace(',', '').replace('ratings', '').replace('rating', '').strip()
 		# Reviews (contains `reviews`).
-		num_reviews_idx = [i for i,s in enumerate(book_metas) if 'reviews' in s]
+		num_reviews_idx = [i for i,s in enumerate(book_metas) if ('reviews' in s or 'review' in s)]
 		if len(num_reviews_idx):
-			book_num_reviews = book_metas[num_reviews_idx[0]].replace('\n', '').replace(',', '').replace('reviews', '').strip()
+			book_num_reviews = book_metas[num_reviews_idx[0]].replace('\n', '').replace(',', '').replace('reviews', '').replace('review', '').strip()
 		#end
 
 		# Genre(s).
