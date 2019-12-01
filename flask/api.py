76ac06840ab5dfc5518907ca5fcf9248ce960ba8
Daniel <KasumiL5x@gmail.com>
2019-12-01 10:36:13 +0000

    The Flask deployment now functions at a basic level!  There's a lot of cleanup and redundant css/js files, but the important part is that it functions for now!

--- flask/api.py
+++ flask/api.py
@@ -8,11 +8,31 @@ class BookSimilarity(object):
 	def __load_data(self):
 		self.__book_data = pickle.load(open('./data/book_data.pickle', 'rb'))
 		print('Loaded book data.')
+		self.__cos_sim = pickle.load(open('./data/cossim.pickle', 'rb'))
+		self.__title_to_idx = pd.Series(self.__book_data.index, index=self.__book_data['title']) # title -> idx mapping
+		print('Loaded cosine similarity matrix.')
 
 	def search(self, query):
 		return self.__book_data.loc[self.__book_data['title'].str.contains(query, case=False)]
 
+	def recommend(self, title):
+		if title not in self.__title_to_idx:
+			print('Title not found in index mapping.')
+			return None
+
+		# Get the index of the passed in book's title.
+		book_idx = self.__title_to_idx[title]
+
+		# Get scores from the cosine similarity matrix for this index.
+		scores = pd.Series(self.__cos_sim[book_idx]).sort_values(ascending=False)
+		# Get the indices of the top 10 books (sub the first as it's the input book).
+		indices = list(scores.iloc[1:11].index)
+
+		return self.__book_data.iloc[indices]
+
 # Offline test.
 # bs = BookSimilarity()
-# res = bs.search('hitch')
-# print(res.title.values)
+# search = bs.search('hitch').iloc[0].title
+# print(f'Search: {search}')
+# res = bs.recommend(search)
+# print(res)
--- flask/app.py
+++ flask/app.py
@@ -17,3 +17,22 @@ def autocomplete():
 	search = flask.request.args.get('q')
 	filtered_books = list(booksim.search(search)['title'].values)
 	return flask.jsonify(matching_results=filtered_books)
+
+@app.route('/recommend')
+def recommend():
+	# Dynamic page help:
+	# https://stackoverflow.com/questions/40963401/flask-dynamic-data-update-without-reload-page/40964086
+	
+	searchText = flask.request.args.get('jsdata')
+
+	output = []
+	if searchText:
+		print(f'Search text: {searchText}')
+		results = booksim.recommend(searchText)
+		if results is not None:
+			output = results.title.values
+			print(output)
+
+	# TODO: Convert a fuller version to JSON rather than just an array (title, url, etc.) and render as a table instead.
+	# https://stackoverflow.com/questions/48050769/pandas-dataframe-to-flask-template-as-a-json
+	return flask.render_template('results.html', recommendations=output)
